9f00e7c8845088ad47f5a9481ddb333b3d29c399
stefanofa <9802152+stefanofa@users.noreply.github.com>
2026-01-22 17:01:10 +0100

    feat: setup example fonts

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,9 +1,28 @@
 import "@/global.css";
+import { JetBrainsMono_400Regular } from "@expo-google-fonts/jetbrains-mono";
+import {
+  Nunito_300Light,
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+  Nunito_900Black,
+} from "@expo-google-fonts/nunito";
+import {
+  Recursive_400Regular,
+  Recursive_700Bold,
+} from "@expo-google-fonts/recursive";
 import { PortalHost } from "@rn-primitives/portal";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { AppThemeProvider } from "@/contexts/app-theme-context";
+
+preventAutoHideAsync();
 
 export const unstable_settings = {
   initialRouteName: "(drawer)",
@@ -22,6 +41,29 @@ function StackLayout() {
 }
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Nunito_300Light,
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+    Nunito_900Black,
+    Recursive_400Regular,
+    Recursive_700Bold,
+    JetBrainsMono_400Regular,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <KeyboardProvider>
